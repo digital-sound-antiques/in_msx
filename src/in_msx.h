8f0e249b035f8ce03186438eac0d045f0faf14a1
Mitsutaka Okazaki <okaxaki@users.noreply.github.com>
2016-05-02 21:54:41 +0900

    Update Version and license info.

--- src/in_msx.h
+++ src/in_msx.h
@@ -4,17 +4,16 @@
 #include "winamp/in2.h"
 
 #define PLUGIN_NAME "in_msx"
-#define PLUGIN_VERSION "0.35"
+#define PLUGIN_VERSION "0.36"
 #define ILL_NAME "in_msx.ill"
 
 #define IN_MSX_ABOUT \
 "MSXplug version " PLUGIN_VERSION " (" __DATE__ ")\r\n" \
-"(c) 1999-2015 Digital Sound Antiques\r\n" \
+"(c) 1999-2016 Digital Sound Antiques\r\n" \
 "http://www.github.io/digital-sound-antiques\r\n" \
 "\r\n" \
 "This software is built with following software.\r\n" \
 "\r\n" \
-"ZLIB1.2.1 (c) Jean-loup Gailly and Mark Adler\r\n" \
 "KMZ80 written by Mamiya\r\n" \
 "MGSDRV (c) GIGAMIX/Ain\r\n" \
 "Kinrou5 (c) Keiichi Kuroda\r\n"
